cffbcc0af4ea8fa9e800173837cdb8be7b35640d
Haseeb Ahmed Mangnejo <143968943+HaseebAhmed-003@users.noreply.github.com>
2026-04-30 06:08:20 +0500

    Update api.js

--- event-frontend/src/api.js
+++ event-frontend/src/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL
-            ? `${process.env.REACT_APP_API_URL}/api`
-            : 'http://localhost:3000/api',
+    ? `${process.env.REACT_APP_API_URL}/api`
+    : 'http://localhost:3000/api',
 });
 
 API.interceptors.request.use((config) => {
